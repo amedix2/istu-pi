e260a07594f34687ea8e4555b631d50364c19448
amedix-mac <amedix2@gmail.com>
2025-11-26 16:15:39 +0400

    added sort method body

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 class Sorter:
     def sort(self, data: list) -> list:
-        ...
+        for i in range(len(data)):
+            for j in range(len(data) - 1):
+                if data[j] > data[j + 1]:
+                    data[j], data[j + 1] = data[j + 1], data[j]
+        return data
 
 
 if __name__ == "__main__":
