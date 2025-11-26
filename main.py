19999c9e1a03379b079d90ddd2ff4dfc7876744c
amedix-mac <amedix2@gmail.com>
2025-11-26 16:19:45 +0400

    sorting algorithm optimization

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 class Sorter:
-    def sort(self, data: list) -> list:
-        for i in range(len(data)):
-            for j in range(len(data) - 1):
+    @staticmethod
+    def sort(data: list) -> list:
+        for i in range(len(data) - 1):
+            swapped = False
+            for j in range(len(data) - i - 1):
                 if data[j] > data[j + 1]:
                     data[j], data[j + 1] = data[j + 1], data[j]
+                    swapped = True
+            if not swapped:
+                break
         return data
 
 
